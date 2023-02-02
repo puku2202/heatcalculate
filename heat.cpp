7391a316df89a9374e241e5911cba07844a5eee8
puku2202 <m1834@s.akashi.ac.jp>
2023-02-02 10:11:50 +0900

    ver.2

--- heat.cpp
+++ heat.cpp
@@ -2,29 +2,31 @@
 #include <sstream> 
 #include <fstream>
 #include<cmath>
+#include<cstring>
 using namespace std;
-ofstream ofs("output.csv");
+//ofstream ofs("output.csv");
 
 double gaussf(double wr, double count, double p){
 	return 2.0*p/M_PI/wr*exp(-2.0*count/wr);
 	}
 int main(){
 	int a;
-	int p[200]=0;
-	double r=27.0/127.0
-	double CDL[200]=0;
+	int p[200]={};
+	char fname[30];
+	double r=27.0/127.0;
+	double CDL[200]={};
 	double x=0, y=0, count=0, g=0;
 	double XR=r, YR=r;
 	double xy[500][25]={0}, X[500]={0}, Y[250]={0};
 	double wr=pow(r,2);
        	double dx, dy=2.0*r/250.0;
-	printf("条件数を入力￥ｎ")；
-	scanf("%d",a);
+	printf("条件数を入力\n");
+	scanf("%d", &a);
 	// CSVファイルを開く
-	FILE * fp = fopen( "input.csv" , "r" ) ;
+	FILE * fp = fopen( "input.csv" , "r" );
 	// 例外
 	if( fp == NULL ) {
-		printf( "Error! File can not be opened\n" ) ;
+		printf( "Error! File can not be opened\n" );
 		return -1;
 	}
 	for(int b=0; fscanf(fp, "%d,%lf\n",&p[b], &CDL[b]) != EOF; b++){//出力，CDL値
@@ -57,6 +59,9 @@ int main(){
 			x=0;
 			XR+=dx;
 		}
+		sprintf(fname, "output%d.csv", h);	
+		ofstream ofs(fname);
+		//FILE * ofs = fopen( "output%d.csv" , "w", &h ) ;	
 		ofs << " " << ", " << "y" << endl;
 		ofs << "x" << ", ";
 		for(int i=0; i<250; i++){
@@ -70,7 +75,8 @@ int main(){
 			}
 			ofs << endl;
 		}
-	
+		memset(fname, '\0', sizeof(fname));
+	}	
 	//printf("%lf",xy[0][0]);
 	return 0;
 }
